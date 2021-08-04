dd3e5d31b838dfabde9142021985a8c4983e2ddf
Breno Augusto <breno.augustosm@gmail.com>
2021-08-04 13:36:37 -0300

    Added integration_test only

---
+++ integration_test/search_test.dart
@@ -0,0 +1,46 @@
+import 'package:clean_mello/modules/users/presentation/widgets/card.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:clean_mello/main_automation.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  group('Search', () {
+    testWidgets("user can search users", (WidgetTester tester) async {
+      // Carrega o app
+      app.main();
+
+      // Espera as animações iniciais terminarem
+      await tester.pumpAndSettle();
+
+      // Espero que exista um widget com a key "list" visível
+      final listFinder = find.byKey(Key('list'));
+      expect(listFinder, findsOneWidget);
+
+      // Espero conseguir clicar no botão com key "search"
+      final searchButtonFinder = find.byKey(Key('search'));
+      await tester.tap(searchButtonFinder);
+
+      // Espero as animações terminarem
+      await tester.pumpAndSettle();
+
+      // Espero que exista um widget com key "list" novamente
+      expect(listFinder, findsOneWidget);
+
+      // Espero achar um ou mais widgets do tipo CardUsers
+      final cardsFinder = find.byType(CardUsers);
+      expect(cardsFinder, findsWidgets);
+
+      // Espero conseguir clicar em um botão com key "clean"
+      final cleanFinder = find.byKey(Key('clean'));
+      await tester.tap(cleanFinder);
+
+      // Espero as animações terminarem
+      await tester.pumpAndSettle();
+    });
+  });
+}
--- integration_test/steps/common/common.dart
+++
@@ -1,5 +0,0 @@
-export 'given_that_there_exists.dart';
-export 'when_i_tap.dart';
-export 'and_i_tap.dart';
-export 'then_i_should_see.dart';
-export 'and_i_should_see_cards.dart';
